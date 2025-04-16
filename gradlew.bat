73be6778d69c500acdcbd95e7a5166f664e2b83e
AdarshGuptaa <adarshguptatiger2006@gmail.com>
2025-04-17 02:47:50 +0530

    Added CustomUserDetails class from spring security

--- gradlew.bat
+++ gradlew.bat
@@ -13,11 +13,6 @@
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
 @rem
-<<<<<<< HEAD
-=======
-@rem SPDX-License-Identifier: Apache-2.0
-@rem
->>>>>>> new
 
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
--- src/main/java/com/passwordmanager/password/manager/user/User.java
+++ src/main/java/com/passwordmanager/password/manager/user/User.java
@@ -1,10 +1,75 @@
 package com.passwordmanager.password.manager.user;
 
 
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name='user_details')
+@Table(name="user_details")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long id;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="created_at")
+    @CreatedDate
+    private Instant creationDate;
+
+    public User(Long id, String email, String password, Instant creationDate) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.creationDate = creationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Instant creationDate) {
+        this.creationDate = creationDate;
+    }
+
+
+
 
 }
