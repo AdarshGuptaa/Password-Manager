9373f67b9b5f6a8dbdd6ad3503ff7b7859c93c84
AdarshGuptaa <adarshguptatiger2006@gmail.com>
2025-04-19 20:16:04 +0530

    Updated jwt dependencies to newer version

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,9 +28,9 @@ dependencies {
 	implementation("mysql:mysql-connector-java:8.0.33")
 
 	// JWT Dependencies
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
 }
 
